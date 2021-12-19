3bb1b03dbd59f0e89ec038c6db48ccc8ae045bd0
fanghs <fanghs@citycloud.com.cn>
2021-12-19 16:02:39 +0800

    添加类型定义

--- src/screens/project-list/SearchPanel.tsx
+++ src/screens/project-list/SearchPanel.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { useEffect, useState } from "react"
-export const SearchPanel = ({ param, setParam, users }) => {
 
+export interface User {
+	id: string
+	name: string
+	email: string
+	title: string
+	organization: string
+}
+
+interface SearchPanelProps {
+	users: User[],
+	param: {
+		name: string,
+		personId: string
+	},
+	setParam: (param: SearchPanelProps['param']) => void
+}
+
+export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
 
 	return (
 		<form>
@@ -13,7 +30,7 @@ export const SearchPanel = ({ param, setParam, users }) => {
 				})}
 			/>
 			<select
-				value={param.id}
+				value={param.personId}
 				onChange={evt => setParam({
 					...param,
 					personId: evt.target.value
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value) => value === 0 ? false : !value
+export const isFalsy = (value: any) => value === 0 ? false : !value
 
 // 不要改变函数的入参
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
     const result = { ...object }
     Object.keys(result).forEach(key => {
+        // @ts-ignore
         const val = result[key]
         if (isFalsy(val)) {
+            // @ts-ignore
             delete result[key]
         }
     })
     return result
 }
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
     const [debounceValue, setDebounceValue] = useState(value)
 
     useEffect(() => {
